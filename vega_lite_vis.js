168bb72e1f1782414bd7563a8859f7bd33b16d67
kongman99 <kongman@sinegy.com>
2021-10-10 12:53:52 +0800

    week10 assessment

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,19 @@
 var vg_1 = "healthy_life_expectancy.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
- // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+
+var vg_2 = "flowchart.vg.json";
+
+
+vegaEmbed('#vis1', vg_1, {"actions": false});
+
+vegaEmbed('#vis2', vg_2, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis3', vg_1, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis4', vg_2, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
