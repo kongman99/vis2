48ababc845bc28f4a0a53625b47054878f3d3c2a
kongman99 <kongman@sinegy.com>
2021-10-14 14:43:24 +0800

    Updated radial chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,6 @@
 var vg_1 = "healthy_life_expectancy.vg.json";
-
-
 var vg_2 = "flowchart.vg.json";
-
+var vg_2 = "radial_chart.vg.json";
 
 vegaEmbed('#vis1', vg_1, {"actions": false});
 
@@ -10,10 +8,6 @@ vegaEmbed('#vis2', vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#vis3', vg_1, {"actions": false}).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-vegaEmbed('#vis4', vg_2, {"actions": false}).then(function(result) {
+vegaEmbed('#vis3', vg_3, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
